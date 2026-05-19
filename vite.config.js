5c67cde743c186272614ce5252ec0096edf0d2e4
techpearlcreator <techpearlcreator@gmail.com>
2026-05-19 21:38:09 +0530

    Configure GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/kannaiah.dev/',
   server: {
     host: 'localhost',
     port: 5173,
